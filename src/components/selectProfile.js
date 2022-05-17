baac008ec2426359685acb8289a4ded572d68481
pagodingo <daniel.danielarchibald@gmail.com>
2022-05-16 23:51:54 -0400

    c

--- src/components/selectProfile.js
+++ src/components/selectProfile.js
@@ -1,20 +1,34 @@
 const selectProfile = ({createNewProfile}) => {
-    return (
-        <div>
-            <h2>Who's watching?</h2>
-            <div className="media-container">
-                <div className="media-group">
-                {JSON.parse(localStorage.getItem("users")).map(user => {
-                return <div className="media-element">
-                    <img src={user.profile}></img>
-                    <p>{user.name}</p>
+    if (localStorage.getItem("users") != null) {
+        return (
+            <div>
+                <h2>Who's watching?</h2>
+                <div className="media-container">
+                    <div className="media-group">
+                    {JSON.parse(localStorage.getItem("users")).map(user => {
+                    return <div className="media-element">
+                        <img src={user.profile}></img>
+                        <h6>{user.permissions}</h6>
+                        <h3>{user.name}</h3>
+                        </div>
+                })}
+                <button className="new-profile" onClick={createNewProfile}>+ new profile</button>
                     </div>
-            })}
                 </div>
             </div>
-            <button onClick={createNewProfile}>create new profile</button>
-        </div>
-    )
+        )
+    } else {
+        return (
+            <div>
+                <h2>Who's watching?</h2>
+                <div className="media-container">
+                    <div className="media-group">
+                        <button className="new-profile" onClick={createNewProfile}>+ new profile</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default selectProfile;
