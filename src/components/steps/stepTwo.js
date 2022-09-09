2f20285e9bb665e2d8bfa72fd51f232150c15da0
pagodingo <daniel.danielarchibald@gmail.com>
2022-09-09 15:15:27 -0400

    -

--- src/components/steps/stepTwo.js
+++ src/components/steps/stepTwo.js
@@ -4,7 +4,7 @@ const stepTwo = ({ prev, inputName }) => {
         <div>
             <h2 className="animate__animated animate__fadeIn">What's your profile name?</h2>
             <form onSubmit={(e) => inputName(e)}>
-            <input type="text" class="animate__animated animate__fadeIn"></input>
+            <input spellcheck="false" type="text" class="animate__animated animate__fadeIn"></input>
             </form>
             <div className="flex">
                 <button onClick={prev}>{'<'}</button>
